266a4ea393804a7f6d881039549a92a842d7d9a1
Connor Shea <connor.james.shea@gmail.com>
2019-02-05 16:39:10 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,78 @@
+# VersionOne to GitLab Issues Importer
+
+This is a Ruby script I wrote to facilitate moving between a project 'backlog' in VersionOne and issues in GitLab. It should cover most major properties you'd want to import from VersionOne tickets. I've tried to comment it well and make it somewhat flexible. Hopefully it's useful to someone!
+
+## Tested with
+
+This script is only tested to work on macOS/Linux. Running it on Windows may work, but it wasn't tested.
+
+- Ruby 2.6 (December 2018)
+- `gitlab` gem v4.9.0 (February 2019, GitLab API v4)
+- GitLab Community Edition v11.7.4 (January 2019)
+
+Note: The script may work with different versions of the above software, but there are no guarantees. Ruby 2.5 should almost definitely work. It _should_ work with GitLab Enterprise Edition/GitLab.com as well, assuming they're on the same or similar version number.
+
+## Data that is imported
+
+The script maps the following data between VersionOne and GitLab Issues:
+
+- Titles are mapped to titles
+- Descriptions are mapped to descriptions
+  - Images in descriptions will not be imported properly.
+  - There are some small formatting issues with descriptions.
+  - Tasks and tests belonging to the ticket are included in the description with their VersionOne title, description, and ID. They're rendered as a task list.
+- Owners are mapped to assignees
+  - Note that, because the script was written for use with Community Edition, multiple assignees are not supported. Only the first owner will be imported. The script should be relatively easy to update if you want to support multiple assignees (please open an issue with your code if you do update the script to make this happen!).
+- Sprints are mapped to Milestones
+- Priorities are mapped to "Low Priority", "Medium Priority", and "High Priority" Labels
+- Backlog Groups are mapped to Labels with the same name as the Backlog Group
+- Statuses are mapped to whether the issue is open/closed
+  - A lack of Status and "In Progress" are both mapped to "Open", "Done" and "Accepted are mapped to "Closed".
+  - An issue is also marked "Closed" if it has anything in the "Closed by" column.
+- Time tracking data ("Todo Hrs." and "Done Hrs.") are mapped to time tracking in GitLab issues.
+- "Create Date" is mapped to the creation date (VersionOne doesn't track specific times, just dates, so it's just set to midnight UTC on the day VersionOne gives. If you need to guarantee the dates are always accurate and never off by a day (GitLab renders dates in your local timezone, so if you're west of UTC you'll get the day before the actual creation date), you'll need to modify the code.)
+  - Note that in order to create issues with a modified creation date you'll need to either be an Owner on the project or need to be an Admin of the GitLab instance.
+
+All labels and milestones will be created for the project, users need to be created in GitLab before running the script, and the VersionOne users need to be mapped manually to their respective usernames in GitLab (see the instructions for more info).
+
+All data from the CSV will be included in the description behind an HTML `<details>` element (collapsed by default) regardless of whether it's listed above. The only exceptions to this are for descriptions (due to descriptions frequently having linebreaks) and for task metadata. Only main ticket metadata is included.
+
+## Instructions
+
+**NOTE**: Before running this on the actual project, I would _strongly_ recommend you use a test project in GitLab to make sure the script is working properly for you. (Also keep in mind if the project is public or has any users in it, they can/will get emails for imported issues.) You can also test it by setting `create_issues` to false in the config near the top of the script. This will cause the script to output the VersionOne issue data and the GitLab issue data into the terminal, but not create any of the actual issues. (It will still create milestones, however!).
+
+1. Clone this repository with `git clone https://github.com/connorshea/versionone-to-gitlab`.
+1. Install Ruby, preferably v2.6.x but other versions will probably work.
+1. If you're not on Bundler 2.x (`bundle --version`), install Bundler 2.0.1 with `gem install bundler:2.0.1`. (You can probably use any version from the 2.x series.)
+1. Install the necessary Ruby gems using `bundle install`.
+1. In VersionOne:
+   - If you'd like to include closed issues, check "Show Closed Items" in the search filter.
+   - If you'd like to include tasks and tests, check "Show Tasks and Tests" in the search filter.
+   - Add all necessary data to the view by going to the issue list, clicking on the wrench icon, and then "Customize". A popup window will open that will let you customize the data shown in the issue list. You'll want the following columns (order doesn't matter, if you have any extra columns that also doesn't matter (extra columns will still be included in the description in a `<details>` tag) so long as all of these are present):
+     - ID
+     - Title
+     - Description
+     - Owner
+     - Backlog Group
+     - Priority
+     - Sprint
+     - Status
+     - Closed By, if you want to include closed issues.
+     - Closed Date
+     - Create Date
+     - Todo Hrs. and Done Hrs, if you want to import time tracking data.
+1. Now open the wrench menu again and click "Export (.xls)", this will create an Excel spreadsheet with all this data.
+1. Open the `.xls` file you've downloaded in Excel, or any compatible spreadsheet tool, and then save/export it as a CSV (use UTF-8 formatting if you can). See [versionone-example.csv](versionone-example.csv) for an example of what the exported CSV file might look like.
+1. Near the top of the `versionone-to-gitlab.rb` Ruby script, modify the `@config` and `@name_hash` variables as needed. The file has comments that should tell you what to put in these variables. If you don't know what you're doing, _don't edit anything below the big warning comment_. If you do know what you're doing, have fun!
+1. Change the `@name_hash` variable in the script such that every key is the user's name in VersionOne and every value is their username on GitLab (the key/value looks like this on each line: `'key': 'value'`). You'll need to manually list everyone who has worked on your project for the owners to be imported properly, and only the first owner will be imported into GitLab Issues.
+1. Set the following environment variables when you run the script, e.g. `GITLAB_PRIVATE_TOKEN=abc123456 ruby versionone-to-gitlab.rb`:
+   - You can also set the environment variables in your command line separately so you don't need to enter the info multiple times.
+   - `GITLAB_PRIVATE_TOKEN`: Your Personal Access Token (more info: https://docs.gitlab.com/ce/user/profile/personal_access_tokens.html, it'll need API permissions)
+   - `GITLAB_API_URL` (default is `https://gitlab.com/api/v4`): The v4 API URL for your GitLab instance.
+   - `GITLAB_PROJECT_ID`: The GitLab "Project ID" number, should be available on the project's home page in GitLab.
+   - `VERSIONONE_CSV`: The name of the VersionOne CSV file you exported from Excel.
+1. Run the script with something like this: `GITLAB_PRIVATE_TOKEN=xxxxx GITLAB_API_URL=https://example.com/api/v4 GITLAB_PROJECT_ID=123 VERSIONONE_CSV=versionone.csv ruby versionone-to-gitlab.rb`, or just `ruby versionone-to-gitlab.rb` if you've set the environment variables separately.
+
+## License
+
+This script is licensed under the terms of the [MIT License](https://opensource.org/licenses/MIT), see [LICENSE](LICENSE).
---
+++ versionone-to-gitlab.rb
@@ -0,0 +1,372 @@
+require 'csv'
+require 'gitlab'
+require 'date'
+
+# Configuration settings that can change the behavior of the script.
+@config = {
+  # Set this to false when testing, to make sure issues aren't actually created.
+  # Milestones will still be created regardless.
+  create_items: true,
+  # Set this to false if you're getting SSL errors (generally avoid setting this to false if you can).
+  ssl_verification: false,
+  # Set a "VersionOne import" label by default for all imported issues.
+  default_labels: ['VersionOne Import']
+}
+
+# A hash of names that is used to map between a user's name in VersionOne
+# and a username in GitLab. Unfortunately there's not a great way to do this
+# automatically, so you'll need to do it manually.
+@name_hash = {
+  'Connor Shea': 'connorshea',
+  'John Doe': 'johndoe',
+  'Jane Doe': 'janedoe'
+}
+
+#############################################
+#############################################
+####   ONLY EDIT DATA ABOVE THIS BLOCK   ####
+#### (unless you know what you're doing) ####
+#############################################
+#############################################
+
+# Your GitLab instance's API URL. Generally this is your instance domain plus `/api/v4`.
+API_URL = ENV['GITLAB_API_URL'] || "https://gitlab.com/api/v4"
+# Your private token, it needs to have enough privileges to create issues, milestones, and labels in the target project.
+PRIVATE_TOKEN = ENV['GITLAB_PRIVATE_TOKEN']
+# GitLab "Project ID", should be available on the project home page in GitLab.
+PROJECT_ID = ENV['GITLAB_PROJECT_ID']
+# CSV has to be in the same directory.
+CSV_FILE_NAME = ENV['VERSIONONE_CSV']
+
+# Create the GitLab client we'll be interacting with the API from.
+@gitlab = Gitlab.client(
+  endpoint: API_URL,
+  private_token: PRIVATE_TOKEN,
+  httparty: { verify: @config[:ssl_verification] }
+)
+
+# Duplicate the name_hash so when we modify it, so it won't lose any data.
+@new_name_hash = @name_hash.dup
+
+# Get GitLab user IDs from each user in @name_hash.
+@name_hash.each do |key, value|
+  # Search for a user based on the username provided in the name hash.
+  user = @gitlab.user_search(value).first
+  # If we can't find a user, skip this name.
+  next if user.nil?
+  user = user.to_hash
+  # Set the value for each property in the hash to their GitLab user id, 
+  # the User id is necessary for the API since it doesn't accept usernames.
+  @new_name_hash[key] = user["id"] 
+end
+
+@name_hash = @new_name_hash
+
+# Convert from a name to the id via the @name_hash.
+def name_converter(name)
+  return @name_hash[:"#{name}"]
+end
+
+@milestones = {}
+
+# Create a milestone for each unique sprint in the issues array.
+def create_milestones(issues)
+  # Create a list of issues with unique sprint names.
+  uniq_issues = issues.uniq { |issue| issue[:sprint] }
+  # Create an array of sprint names.
+  sprints = uniq_issues.map { |issue| issue[:sprint] }
+  # Remove any empty strings.
+  sprints = sprints.reject { |sprint| sprint.nil? || sprint.empty? }
+
+  # Iterate through each sprint name in the array.
+  sprints.each do |sprint|
+    # Search for all existing milestones in the project with the same name as the current sprint.
+    existing_milestones = @gitlab.milestones(PROJECT_ID, options: { search: "#{sprint}" } )
+
+    # Create a list of existing milestones.
+    existing_milestones_array = []
+    existing_milestones.auto_paginate do |_milestone|
+      existing_milestones_array << _milestone.to_hash
+    end
+
+    # Select any milestone with the same title as the current sprint.
+    existing_sprint = existing_milestones_array.select do |_milestone|
+      next(_milestone.to_hash["title"] == sprint)
+    end
+
+    # If the search returned any milestone with the same title as the current
+    # sprint, set the milestone variable to that Milestone object.
+    if !existing_sprint.empty?
+      milestone = existing_sprint.first.to_hash
+    # Otherwise, create a new milestone with the given sprint's name.
+    else
+      milestone = @gitlab.create_milestone(PROJECT_ID, "#{sprint}")
+      milestone = milestone.to_hash
+    end
+
+    # Map between the sprint name and milestone id in GitLab.
+    @milestones["#{sprint}"] = milestone["id"]
+  end
+end
+
+# Truncate long strings to a max of 100 characters.
+# Only used for printing to the console while running the script.
+def truncate(s, length = 100, ellipsis = '...')
+  if s.length > length
+    s.to_s[0..length].gsub(/[^\w]\w+\s*$/, ellipsis)
+  else
+    s
+  end
+end
+
+# Print metadata for the VersionOne ticket and the GitLab issue.
+# Used for the command line interface when running the script.
+def print_issue_metadata(issue, gitlab_hash, issue_title)
+  puts
+  puts "------"
+  puts
+  puts "\e[4mVersionOne Issue Metadata:\e[24m"
+  issue.each do |key, value|
+    # For strings like the description, remove any linebreaks from the end
+    # and truncate them to 100 characters.
+    if value.is_a?(String)
+      puts "\e[1m#{key.to_s}\e[22m: #{truncate(value).chomp}"
+    else
+      puts "\e[1m#{key.to_s}\e[22m: #{value}"
+    end
+  end
+  puts
+  puts "\e[4mGitLab Issue Metadata:\e[24m"
+  puts "\e[1mtitle\e[22m: #{issue_title}"
+  gitlab_hash.each do |key, value|
+    # For strings like the description, remove any linebreaks from the end
+    # and truncate them to 100 characters.
+    if value.is_a?(String)
+      puts "\e[1m#{key.to_s}\e[22m: #{truncate(value).chomp}"
+    else
+      puts "\e[1m#{key.to_s}\e[22m: #{value}"
+    end
+  end
+end
+
+# Humanize time into a format that GitLab accepts.
+# Inputs an integer or float and returns a time in the format of "3h30m".
+# Examples:
+# 1.00 => 1h
+# 1.50 => 1h30m
+# 0.50 => 30m
+# 1 => 1h
+# 25.00 => 25h
+# 1.25 => 1h15m
+# 
+# Note that GitLab will auto-convert values of 8h to 1d in their interface (it assumes a standard 9-5 work day).
+# e.g. 9h => 1d1h, 20h => 2d4h.
+def humanize_time(time)
+  if time % 1 == 0
+    return "#{time.floor}h"
+  elsif time < 1
+    return "#{(time * 60).floor}m"
+  else
+    return "#{time.floor}h#{((time % 1) * 60).floor}m"
+  end
+end
+
+# Create an empty array of rows to fill with the CSV data.
+rows = []
+
+# Scan the supplied CSV and create an array of row hashes.
+CSV.foreach(
+  File.join(File.dirname(__FILE__), CSV_FILE_NAME),
+  skip_blanks: true,
+  headers: true,
+  header_converters: :symbol,
+  encoding: 'bom|utf-8'
+) do |row|
+  rows << row
+end
+
+# Track the most recent ticket.
+most_recent_parent_item = nil
+
+# Iterate through each row to add task information to each row.
+# The CSV is organized such that the tickets always come before their associated
+# tasks, so we can make some assumptions about how the tickets and tasks are associated.
+rows.each_with_index do |row, index|
+  # Identify rows which are tasks.
+  if row[:title].nil?
+    row[:title] = row[1]
+    row[:task] = true
+  else
+    row[:task] = false
+  end
+
+  # Delete nil data.
+  row.delete_if { |key, value| key.nil? }
+
+  if row[:task]
+    row[:parent_item] = most_recent_parent_item
+  else
+    most_recent_parent_item = index
+  end
+
+  rows[index] = row
+end
+
+# Create an empty array of issues so we can fill it after parsing the rows.
+issues = []
+
+missing_labels = []
+
+# Iterate through each row to create issue hashes.
+rows.each_with_index do |row, index|
+  # Skip the row if it represents a task
+  next if row[:task]
+
+  # Add the closing date to the issue description if the issue has a closed_date value.
+  description = row[:closed_date].nil? ? "**VersionOne ID: #{row[:id]}**\n\n#{row[:description]}" : "**VersionOne ID: #{row[:id]}**\n\n**Closed date**: #{row[:closed_date]}\n\n#{row[:description]}"
+
+  # Create a hash with various properties from the CSV columns.
+  issue = {
+    id: row[:id],
+    title: row[:title],
+    priority: row[:priority],
+    description: description,
+    sprint: row[:sprint],
+    status: row[:status],
+    backlog_group: row[:backlog_group],
+    closed_by: row[:closed_by],
+    create_date: row[:create_date],
+    done_hrs: row[:done_hrs],
+    todo_hrs: row[:to_do_hrs]
+  }
+
+  # Convert the issue owner to their user id, this only allows the first owner
+  # to be assigned because GitLab CE doesn't support multiple assignees.
+  issue[:owner] = name_converter(row[:owner].split(',').first) unless row[:owner].nil?
+
+  # Filter the rows to child tasks of the current issue.
+  tasks = rows.select { |row| row[:parent_item] == index }
+  # If the current issue has any child tasks, render them in the description.
+  if tasks.length > 0
+    issue[:description] += "\n\n### Tasks and Tests"
+    tasks.each do |task|
+      # Determine whether the checkbox should be checked based on the task status.
+      if task[:status] == "Accepted" || task[:status] == "Completed" || task[:status] == "Done"
+        completed = "x"
+      else
+        completed = " "
+      end
+
+      # Add the task to the current issue description
+      issue[:description] += "\n- [#{completed}] **#{task[:id]}**: #{task[:title]}"
+      # Add the task description in a code block if the task has a description.
+      unless task[:description].nil? || task[:description] == ""
+        issue[:description] += "\n```\n#{task[:description]}\n```"
+      end
+    end
+  end
+
+  # Add VersionOne metadata in a details tag at the bottom of the description.
+  issue[:description] += "\n<details>\n<summary>VersionOne Issue Metadata</summary>\n"
+  row.each do |key, value|
+    # Exclude the description and task data, and columns with no name
+    unless key == :description || key == :task || key.nil?
+      issue[:description] += "\n- #{key.to_s}: #{value}"
+    end
+  end
+  issue[:description] += "\n</details>"
+
+  # Strip any unknown characters from the descriptions.
+  issue[:description] = issue[:description].force_encoding('utf-8').tr("\uFFFD", " ") unless issue[:description].nil?
+
+  # Push the issue hash into the issues array.
+  issues << issue
+end
+
+# Pass the issues array we created to the create_milestones method.
+# This will create an @milestones variable.
+create_milestones(issues)
+
+puts "Creating #{issues.length} issues..."
+# Iterate over each issue and map VersionOne issue attributes to their
+# GitLab counterparts.
+issues.each do |issue|
+  gitlab_hash = {}
+
+  # Map the description to the description
+  gitlab_hash[:description] = issue[:description] unless issue[:description].nil?
+  # Map the first owner to the assignee, GitLab CE only supports one assignee.
+  gitlab_hash[:assignee_ids] = [issue[:owner]] unless issue[:owner].nil?
+  # Map sprints to milestones
+  gitlab_hash[:milestone_id] = @milestones[issue[:sprint].gsub(/Sprint ([A-Z])/, 'Sprint 17\1')] unless issue[:sprint].nil?
+  # Set a custom creation date.
+  gitlab_hash[:created_at] = DateTime.strptime(issue[:create_date], '%m/%d/%y').iso8601 unless issue[:create_date].nil?
+
+  labels = []
+  # Map Priority information to Low/Medium/High Priority labels
+  labels << "#{issue[:priority]} Priority" unless issue[:priority].nil?
+  # Add a label of the same time if the issue was in a backlog group
+  labels << "#{issue[:backlog_group]}" unless issue[:backlog_group].nil?
+  # Add custom labels if there are any
+  unless @config[:default_labels].length == 0
+    @config[:default_labels].each do |label|
+      labels << label
+    end
+  end
+
+  # Create a string with each label seprated by a comma, if there's more than one.
+  gitlab_hash[:labels] = labels.join(',') unless labels.empty?
+
+  if issue[:title].nil? || issue[:title].empty?
+    issue_title = issue[:id]
+  else
+    issue_title = issue[:title]
+  end
+
+  # Create the issue using the options defined above.
+  if @config[:create_items]
+    print_issue_metadata(issue, gitlab_hash, issue_title)
+
+    # Create the issue.
+    gitlab_issue = @gitlab.create_issue(
+      PROJECT_ID,
+      issue_title,
+      gitlab_hash
+    )
+
+    # Translate Todo Hrs and Done Hrs in VersionOne to time tracking info in GitLab.
+    # If both todo_hrs and done_hrs are nil, skip handling time tracking for this issue.
+    if (issue[:todo_hrs] || issue[:done_hrs])
+      # If either todo_hrs or done_hrs are nil, replace them with 0.
+      issue[:todo_hrs] = 0 if issue[:todo_hrs].nil?
+      issue[:done_hrs] = 0 if issue[:done_hrs].nil?
+
+      # Convert to a float if the time value is a String.
+      issue[:todo_hrs] = issue[:todo_hrs].to_f if issue[:todo_hrs].is_a?(String)
+      issue[:done_hrs] = issue[:done_hrs].to_f if issue[:done_hrs].is_a?(String)
+
+      # The time estimate is the total todo_hrs plus the done_hrs.
+      time_estimate = issue[:todo_hrs] + issue[:done_hrs]
+      # The time spent is the total done_hrs.
+      time_spent = issue[:done_hrs]
+      
+      @gitlab.estimate_time_of_issue(PROJECT_ID, gitlab_issue.to_h['iid'], humanize_time(time_estimate)) unless time_estimate == 0 || time_estimate.nil?
+      @gitlab.add_time_spent_on_issue(PROJECT_ID, gitlab_issue.to_h['iid'], humanize_time(time_spent)) unless time_spent == 0 || time_spent.nil?
+    end
+
+    # If the issue is marked as "Done", "Accepted", or has been closed in VersionOne, close it.
+    if (issue[:status] == "Done" || issue[:status] == "Accepted" || !issue[:closed_by].nil?)
+      @gitlab.close_issue(PROJECT_ID, gitlab_issue.to_h['iid'])
+    end
+
+  # For testing purposes, print metadata if the create_items config
+  # option is set to false.
+  elsif !@config[:create_items]
+    print_issue_metadata(issue, gitlab_hash, issue_title)
+  end
+end
+
+puts
+puts "------"
+puts "Created #{issues.length} issues."
+puts "Created #{@milestones.length} milestones."
